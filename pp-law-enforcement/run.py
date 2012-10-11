eb8f29164c5931e46723033d5b2af9ca2ee20074
Waleed Ammar <wammar@cs.cmu.edu>
2012-10-11 16:02:57 -0400

    1) major refactoring. 2) add l2 regularization in batch gradient descent.

--- pp-law-enforcement/run.py
+++ pp-law-enforcement/run.py
@@ -16,21 +16,48 @@ import socket
 from collections import defaultdict
 from boilerpipe.extract import Extractor
 
-# parse command line arguments
-pythonScript = sys.argv[0]
-assert(pythonScript[-3:]=='.py')
-rawDir = sys.argv[1]
-outputPrefix = sys.argv[2]
+def SelfLearn(logReg, labeledExamples, unlabeledExamples, testExample):
+  # train the model with labeled examples
+  logReg.MinimizeLossWithSGD(labeledExamples, persistWeightsAt='')
 
-# importing in-progress modules
-featureExtractorModulePath = '/cab0/wammar/exp/feat-ext'
-if featureExtractorModulePath not in sys.path:
-  sys.path.append(featureExtractorModulePath)
-from feature_extractor import FeatureExtractor
-logResModulePath = '/cab0/wammar/exp/log-reg/source'
-if logResModulePath not in sys.path:
-  sys.path.append(logResModulePath)
-from log_reg import LogisticRegression
+  # find unlabeled examples of which we are confident
+  selfLearntExamples = []
+  good1s = 0
+  good0s = 0
+  for unlabeledExampleId in range(0, len(unlabeledExamples)):
+    prob1 = logReg.ComputeProb1(unlabeledExamples[unlabeledExampleId])
+    if prob1 > 0.8:
+      good1s += 1
+#      print 'adding {0}. prob1 = {1}. 1s = {2}'.format(unlabeledExampleId, prob1, good1s)
+      newExample = unlabeledExamples[unlabeledExampleId]
+      newExample.label = 1
+      selfLearntExamples.append(newExample)
+    elif prob1 < 0.01:
+      good0s += 1
+#      print 'adding {0}. prob1 = {1}. 0s = {2}'.format(unlabeledExampleId, prob1, good0s)
+      newExample = unlabeledExamples[unlabeledExampleId]
+      newExample.label = 0
+      selfLearntExamples.append(newExample)
+      
+  # train the log linear model with self learnt examples
+#  print '=============='
+
+#  print '# original training examples = {0}'.format(len(labeledExamples))
+
+#  print 'now, using {0} self learnt +ve examples and {1} self learnt -ve examples, totaling {2} out of {3} unlabeled examples'.format(good1s, good0s, len(selfLearntExamples), len(unlabeledExamples))
+
+#  print 'Use self learnt examples, in addition to labeled ones'
+  labeledExamples.extend(selfLearntExamples)
+
+#  print '# total training examples = {0}'.format(len(labeledExamples))
+  logReg.MinimizeLossWithSGD(labeledExamples, persistWeightsAt='')
+
+  prob1 = logReg.ComputeProb1(testExample)
+#  print '****************************'
+  print '{0}\t{1}'.format(prob1, testExample.label)
+#  print '****************************'
+
+  return
 
 def WriteFeaturesFile(docIdLabelFilename, featuresLabelFilename):
   # for each training example in raw/docId-label, write a line in features/labeled.txt. Instead of the site-ID in the original file, write the non-zero feature IDs and their values.
@@ -63,6 +90,23 @@ def WriteFeaturesFile(docIdLabelFilename, featuresLabelFilename):
   # done with those files
   docIdLabelFile.close()
   featuresLabelFile.close()
+  return
+
+# parse command line arguments
+pythonScript = sys.argv[0]
+assert(pythonScript[-3:]=='.py')
+rawDir = sys.argv[1]
+outputPrefix = sys.argv[2]
+
+# importing in-progress modules
+featureExtractorModulePath = '/cab0/wammar/exp/feat-ext'
+if featureExtractorModulePath not in sys.path:
+  sys.path.append(featureExtractorModulePath)
+from feature_extractor import FeatureExtractor
+logResModulePath = '/cab0/wammar/exp/log-reg/source'
+if logResModulePath not in sys.path:
+  sys.path.append(logResModulePath)
+from log_reg import *
 
 # initialize the feature extractor
 extractor = FeatureExtractor('|')
@@ -71,60 +115,42 @@ extractor = FeatureExtractor('|')
 labeledFeaturesFilename = '{0}.labeled'.format(outputPrefix)
 WriteFeaturesFile('{0}/docId-label.txt'.format(rawDir), labeledFeaturesFilename)
 
+# initialize logistic regressin model
+logReg = LogisticRegression()
+
+# specify learning info
+learningInfo = LearningInfo(
+  learningRate = 0.001, 
+  stoppingCriterion = StoppingCriterion.TRAIN_LIKELIHOOD,
+  stoppingCriterionThreshold = 0.001,
+  regularizationCoefficient = 100, 
+  regularizer = Regularizer.NONE,
+  optimizationAlgorithm = OptimizationAlgorithm.GRADIENT_DESCENT,
+  positiveDevSetSize = 0, 
+  negativeDevSetSize = 0,
+  persistWeightsAtPrefix = outputPrefix)
+
 # now, do LOO cross validation
-logReg = LogisticRegression(labeledFeaturesFilename, learningRate = 0.001, maxIterLossDiff = 0.0001, regularizationConst = 0)
-(precision, recall, accuracy, f1) = logReg.NFoldCrossValidation(20, outputPrefix, 'SGD')
+labeledExamples = logReg.ReadExamples(labeledFeaturesFilename)
+(precision, recall, accuracy, f1) = logReg.NFoldCrossValidation(labeledExamples, 19, learningInfo, 0.5)
 print 'precision:\t{0}\nrecall:\t{1}\naccuracy:\t{2}\nf1:   \t{3}\n'.format(precision, recall, accuracy, f1)
 
 exit(0);
-logReg.MinimizeLossWithSGD(logReg.examples, persistWeightsAt='{0}.optimalFeatureWeightsWithLabeledExamples'.format(outputPrefix))
 
 # for each unlabeled example in raw/docId-randomLabel, write a line in features/randomLabeled.txt.
 unlabeledFeaturesFilename = '{0}.unlabeled'.format(outputPrefix)
-WriteFeaturesFile('{0}/docId-randomLabel.txt'.format(rawDir), unlabeledFeaturesFilename)
+if not os.path.exists('{0}/docId-randomLabel.txt'.format(rawDir)):
+  WriteFeaturesFile('{0}/docId-randomLabel.txt'.format(rawDir), unlabeledFeaturesFilename)
 
-# find unlabeled examples of which we are confident
+# then, read unlabeledExamples
 unlabeledExamples = logReg.ReadExamples(unlabeledFeaturesFilename)
-selfLearntExamples = []
-for unlabeledExampleId in range(0, len(unlabeledExamples)):
-  prob1 = logReg.ComputeProb1(unlabeledExamples[unlabeledExampleId])
-  if prob1 > 0.8:
-    print 'adding {0}. prob1 = {1}'.format(unlabeledExampleId, prob1)
-    newExample = unlabeledExamples[unlabeledExampleId]
-    newExample.label = 1
-    selfLearntExamples.append(newExample)
-  elif prob1 < 0.01:
-    print 'adding {0}. prob1 = {1}'.format(unlabeledExampleId, prob1)
-    newExample = unlabeledExamples[unlabeledExampleId]
-    newExample.label = 0
-    selfLearntExamples.append(newExample)
 
-# train the log linear model with self learnt examples
-print '=============='
-print '# original examples = {0}'.format(len(logReg.examples))
+# leave-one-out with self learning
 labeledExamples = list(logReg.examples)
-logReg.examples = selfLearntExamples
-print '# total examples = {0}'.format(len(logReg.examples))
-logReg.MinimizeLossWithSGD(logReg.examples, persistWeightsAt='')
+for i in range(0, len(labeledExamples)):
+  print 'now using {0} as a test example'.format(i)
+  testExample = labeledExamples[i]
+  seedExamples = labeledExamples[0:i]
+  seedExamples.extend(labeledExamples[i+1:])
+  SelfLearn(logReg, seedExamples, unlabeledExamples, testExample)
 
-truePositives = 0
-trueNegatives = 0
-falsePositives = 0
-falseNegatives = 0
-for labeledExample in labeledExamples:
-  if logReg.ComputeProb1(labeledExample) > 0.5:
-    if labeledExample.label == 1:
-      truePositives += 1.0
-    else:
-      falsePositives += 1.0
-  else:
-    if labeledExample.label == 1:
-      falseNegatives += 1.0
-    else:
-      trueNegatives += 1.0
-
-precision = truePositives / (truePositives + falsePositives)
-recall = truePositives / (truePositives + falseNegatives)
-accuracy = (truePositives + trueNegatives) / (truePositives + trueNegatives + falsePositives + falseNegatives)
-f1 = (precision + recall) / (precision * recall)
-print 'precision:\t{0}\nrecall:\t{1}\naccuracy:\t{2}\nf1:   \t{3}\n'.format(precision, recall, accuracy, f1)
--- source/log_reg.py
+++ source/log_reg.py
@@ -16,6 +16,44 @@ import socket
 from collections import defaultdict
 from boilerpipe.extract import Extractor
 
+class StoppingCriterion:
+  # stop training when |loglikelihood(train data|new model) - loglikelihood(train data|prev model)| < threshold
+  TRAIN_LIKELIHOOD=1 
+  # stop training when loglikelihood(dev data|new model) - loglikelihood(dev data|prev model) < 0
+  DEV_LIKELIHOOD=2
+  
+class OptimizationAlgorithm:
+  STOCHASTIC_GRADIENT_DESCENT=1
+  GRADIENT_DESCENT=2
+
+class Regularizer:
+  NONE=1
+  L2=2
+
+class LearningInfo:
+  def __init__(self, 
+               stoppingCriterion=StoppingCriterion.TRAIN_LIKELIHOOD, 
+               stoppingCriterionThreshold = 0.00001, 
+               positiveDevSetSize = 1, 
+               negativeDevSetSize = 1,
+               optimizationAlgorithm = OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT,
+               learningRate = 0.001,
+               persistWeightsAtPrefix = '',
+               regularizer = Regularizer.NONE,
+               regularizationCoefficient = 0):
+    self.stoppingCriterion = stoppingCriterion
+    self.stoppingCriterionThreshold = stoppingCriterionThreshold
+    self.positiveDevSetSize = positiveDevSetSize
+    self.negativeDevSetSize = negativeDevSetSize
+    self.optimizationAlgorithm = optimizationAlgorithm
+    self.learningRate = learningRate
+    self.persistWeightsAtPrefix = persistWeightsAtPrefix
+    self.regularizer = regularizer
+    self.regularizationCoefficient = regularizationCoefficient
+
+  def REMOVE_ME(self):
+    print 'DONOTHING'
+
 # featuresDict is a defaultdict that maps non-zero feature ID to feature value in this example. label must be 0 or 1
 class Example:
   def __init__(self, featuresDict, label):
@@ -55,26 +93,14 @@ class LogisticRegression:
         newExample = Example(featuresDict, label)
         examples.append(newExample)
       labelFeaturesFile.close()
+
     return examples
 
-  def __init__(self, labelFeaturesFilename, learningRate, maxIterLossDiff, regularizationConst = 0):
-    # regularization const
-    self.regularizationConst = regularizationConst
+  def __init__(self):
 
-    # learning rate
-    self.learningRate = learningRate
-    assert(self.learningRate > 0)
-
-    # convergence criterion
-    self.maxIterLossDiff = maxIterLossDiff
-    assert(self.maxIterLossDiff > 0)
-    
     # feature weights
     self.InitFeatureWeights()
 
-    # read examples in labelFeaturesFilename in a list self.examples
-    self.examples = self.ReadExamples(labelFeaturesFilename)
-            
   # fix exponent (before applying math.exp(exponent)) to avoid math range errors
   def FixExponent(self, exponent):
     if exponent > 600:
@@ -84,12 +110,13 @@ class LogisticRegression:
   # given an unlabeled example, compute probability that label=1, using the current feature weights
   def ComputeProb1(self, unlabeledExample):
     exponent = 0
-#    print '===================computeprob1Init: {0}'.format(exponent)
+#    print '===================\nunlabeledExample: \n{0}'.format(unlabeledExample)
     for featureId in unlabeledExample.featuresDict.keys():
       exponent -= unlabeledExample.featuresDict[featureId] * self.weights[featureId]
 #      print 'computeprob1: {0}'.format(exponent)
     exponent = self.FixExponent(exponent)
     prob = 1.0 / (1.0 + math.exp(exponent))
+    prob = self.fixProb(prob)
     return prob
 
   # slightly adjust probability to avoid math domain errors
@@ -103,102 +130,162 @@ class LogisticRegression:
 
   # minimize the loss function using stochastic gradient descent
   # TODO: make sure L2 regularization works. and refactor the implementation such that other regularizers can be also used
-  def MinimizeLossWithSGD(self, trainExamples, persistWeightsAt=''):
+  def MinimizeLossWithSGD(self, trainExamples, learningInfo, persistWeightsAt=''):
+
+    # only TRAIN_LIKELIHOOD stopping criteria is implemented
+    assert(learningInfo.stoppingCriterion == StoppingCriterion.TRAIN_LIKELIHOOD)
+
+    # only L2 regularizer (and none) is implemented
+    assert(learningInfo.regularizer == Regularizer.L2 or learningInfo.regularizer == Regularizer.NONE)
+
     converged = False
     prevIterLoss = 0
-    # keep updating weights until the total iteration loss stabilizes
+
+    # keep updating weights until the stopping criterion is satisfied
     iterationsCounter = 0
     while not converged:
       iterationsCounter += 1
       iterLoss = 0
+
+      # for each training example
       for example in trainExamples:
+
+        # compute p(class=1|example)
         prob1 = self.ComputeProb1(example)
-        prob1 = self.fixProb(prob1)
+
+        # update the iteration loss
         if example.label == 1:
           iterLoss -= math.log(prob1) / len(trainExamples)
         else:
           iterLoss -= math.log(1-prob1) / len(trainExamples)
+
+        # update the weights of features firing in this example
         for featureId in example.featuresDict.keys():
-          # update the weights
-          l2RegularizationUpdateTerm = self.regularizationConst / len(trainExamples) * self.weights[featureId]
-          self.weights[featureId] -= self.learningRate * ( example.featuresDict[featureId] * (prob1 - example.label) + l2RegularizationUpdateTerm )
+          
+          # compute the contribution of this example towards the l2-regularizer term of this feature 
+          l2RegularizationUpdateTerm = learningInfo.regularizationCoefficient / len(trainExamples) * self.weights[featureId]
+
+          # the actual weight update happens here
+          self.weights[featureId] -= learningInfo.learningRate * ( example.featuresDict[featureId] * (prob1 - example.label) + l2RegularizationUpdateTerm )
+
+        # end of features
+
+      # end of training examples
+
       # now compute the feature vector's L2 norm
       l2RegularizationLossTerm = 0
       for featureId in self.weights.keys():
-        l2RegularizationLossTerm += self.regularizationConst / len(trainExamples) / 2.0 * self.weights[featureId] * self.weights[featureId]
-      iterLoss += self.regularizationConst * l2RegularizationLossTerm
-#      print 'iterLoss = {0}'.format(iterLoss)
-      if math.fabs(iterLoss - prevIterLoss) <= self.maxIterLossDiff:
+        l2RegularizationLossTerm += learningInfo.regularizationCoefficient / len(trainExamples) / 2.0 * self.weights[featureId] * self.weights[featureId]
+
+      # add the L2 norm to the iteration loss
+      iterLoss += l2RegularizationLossTerm
+
+      # determine convergence
+      if math.fabs(iterLoss - prevIterLoss) <= learningInfo.stoppingCriterionThreshold:
         print 'cost function converged after {2} iterations: prevIterLoss = {0} | iterLoss = {1}'.format(prevIterLoss, iterLoss, iterationsCounter)
         converged = True
+      # update prevIterLoss for next iterations
       prevIterLoss = iterLoss
+
+    # stopping criterion reached!
+
     # persist optimal weights
     if len(persistWeightsAt) > 0:
       weightsFile = open(persistWeightsAt, 'w')
       for key in self.weights.keys():
         weightsFile.write('{0}\t{1}\n'.format(key, self.weights[key]))
       weightsFile.close()
-        
+
+  # end of MinimizeLossWithSGD
+
   # minimize the loss function using batch gradient descent
-  # TODO: add L2 regularization works. and refactor the implementation such that other regularizers can be also used
-  def MinimizeLossWithGD(self, trainExamples, persistWeightsAt=''):
+  # TODO: add L2 regularization. and refactor the implementation such that other regularizers can be also used
+  def MinimizeLossWithGD(self, trainExamples, learningInfo, persistWeightsAt=''):
+
+    # only TRAIN_LIKELIHOOD stopping criteria is implemented
+    assert(learningInfo.stoppingCriterion == StoppingCriterion.TRAIN_LIKELIHOOD)
+
+    # no regularizer is implemented none
+    assert(learningInfo.regularizer == Regularizer.NONE)
+
     converged = False
     prevIterLoss = 0
-    # keep updating weights until the total iteration loss stabilizes
+
+    # keep updating weights until stopping criterion
     iterationsCounter = 0
-    gradient = defaultdict[float]
+    gradient = defaultdict(float)
     while not converged:
+
+      # new iteration, new gradient, new loss
       gradient.clear()
       iterationsCounter += 1
       iterLoss = 0
+
+      # for each training example
       for example in trainExamples:
+
+        # compute p(class=1|example) with current model params
         prob1 = self.ComputeProb1(example)
-        prob1 = self.fixProb(prob1)
+
+        # update iteration loss
         if example.label == 1:
           iterLoss -= math.log(prob1) / len(trainExamples)
         else:
           iterLoss -= math.log(1-prob1) / len(trainExamples)
+
+        # for each feature that fire in this example
         for featureId in example.featuresDict.keys():
+
           # update the gradient
           gradient[featureId] += example.featuresDict[featureId] * (prob1 - example.label) 
+        
+        # end of features fired in this example
 
-      # update the weights
+      # end of training examples
+
+      # update the weights with the gradient
       for featureId in gradient.keys():
-        self.weights[featureId] -= self.learningRate * gradient[featureId]
+        self.weights[featureId] -= learningInfo.learningRate * (gradient[featureId] + learningInfo.regularizationCoefficient / len(trainExamples) * self.weights[featureId])
 
       # now compute the feature vector's L2 norm
-#      l2RegularizationLossTerm = 0
-#      for featureId in self.weights.keys():
-#        l2RegularizationLossTerm += self.regularizationConst / len(trainExamples) / 2.0 * self.weights[featureId] * self.weights[featureId]
-#      iterLoss += self.regularizationConst
+      l2RegularizationLossTerm = 0
+      for featureId in self.weights.keys():
+        l2RegularizationLossTerm += learningInfo.regularizationCoefficient / len(trainExamples) / 2.0 * self.weights[featureId] * self.weights[featureId]
+      iterLoss += l2RegularizationLossTerm
 
-#      print 'iterLoss = {0}'.format(iterLoss)
-      if math.fabs(iterLoss - prevIterLoss) <= self.maxIterLossDiff:
+      print 'iterLoss = {0}'.format(iterLoss)
+
+      # stopping criterion
+      if math.fabs(iterLoss - prevIterLoss) <= learningInfo.stoppingCriterionThreshold:
         print 'cost function converged after {2} iterations: prevIterLoss = {0} | iterLoss = {1}'.format(prevIterLoss, iterLoss, iterationsCounter)
         converged = True
       prevIterLoss = iterLoss
+
+    # end of training iterations
+
     # persist optimal weights
     if len(persistWeightsAt) > 0:
       weightsFile = open(persistWeightsAt, 'w')
       for key in self.weights.keys():
         weightsFile.write('{0}\t{1}\n'.format(key, self.weights[key]))
       weightsFile.close()
+
+  # end of MinimizeLossWithGD
         
   # evaluate. returns (falsePositives, falseNegatives, truePositives, trueNegatives)
-  def Evaluate(self, testExamples):
+  def Evaluate(self, testExamples, classThreshold = 0.5):
     falsePositives = 0
     falseNegatives = 0
     truePositives = 0
     trueNegatives = 0
     for example in testExamples:
       prob1 = self.ComputeProb1(example)
-      prob1 = self.fixProb(prob1)
       print 'prob1= {0} | label = {1}'.format(prob1, example.label)
-      if prob1 >= 0.5 and example.label == 1:
+      if prob1 >= classThreshold and example.label == 1:
         truePositives += 1
-      elif prob1 < 0.5 and example.label == 0:
+      elif prob1 < classThreshold and example.label == 0:
         trueNegatives += 1
-      elif prob1 >= 0.5 and example.label == 0:
+      elif prob1 >= classThreshold and example.label == 0:
         falsePositives += 1
       else:
         falseNegatives += 1
@@ -206,40 +293,44 @@ class LogisticRegression:
     return (falsePositives, falseNegatives, truePositives, trueNegatives, prob1)
 
   # n-fold cross validation. returns (precision, recall, accuracy, f1)
-  def NFoldCrossValidation(self, n, persistWeightsAtPrefix = '', optimizationAlgorithm = 'SGD'):
+  def NFoldCrossValidation(self, examples, n, learningInfo, classThreshold = 0.5):
     totalFalsePositives, totalFalseNegatives, totalTruePositives, totalTrueNegatives = 0, 0, 0, 0
     precision, recall, accuracy, f1 = 0, 0, 0, 0
 
     # only stochastic gradient descent and batch gradient descent are implemented
-    assert(optimizationAlgorithm == 'SGD' or optimizationAlgorith == 'GD')
+    assert(learningInfo.optimizationAlgorithm == OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT or 
+           learningInfo.optimizationAlgorithm == OptimizationAlgorithm.GRADIENT_DESCENT)
 
     # find out which foldId each example belongs to
-    examplesPerFold = int(math.ceil(len(self.examples) / float(n)))
+    examplesPerFold = int(math.ceil(len(examples) / float(n)))
     foldIds = []
-    for i in range(0, len(self.examples)):
+    for i in range(0, len(examples)):
       foldIds.append(i/examplesPerFold)
 
     # iterate over the n folds
     for testFoldId in range(0, n):
+
       # identify train vs test examples
       trainExamples, testExamples = [], []
-      for exampleId in range(0, len(self.examples)):
+      for exampleId in range(0, len(examples)):
         if foldIds[exampleId] == testFoldId:
-          testExamples.append(self.examples[exampleId])
+          testExamples.append(examples[exampleId])
         else:
-          trainExamples.append(self.examples[exampleId])
-      # optimize feature weights on train set
+          trainExamples.append(examples[exampleId])
+
+      # optimize feature weights
       self.InitFeatureWeights()
-      if len(persistWeightsAtPrefix) > 0:
-        persistWeightsAt = '{0}.weights.{1}'.format(persistWeightsAtPrefix, testFoldId)
+      if len(learningInfo.persistWeightsAtPrefix) > 0:
+        persistWeightsAt = '{0}.weights.{1}'.format(learningInfo.persistWeightsAtPrefix, testFoldId)
       else:
         persistWeightsAt = ''
-      if optimizationAlgorithm == 'SGD':
-        self.MinimizeLossWithSGD(trainExamples, persistWeightsAt)
-      elif optimizationAlgorithm == 'GD':
-        self.MinimizeLossWithGD(trainExamples, persistWeightsAt)
+      if learningInfo.optimizationAlgorithm == OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT:
+        self.MinimizeLossWithSGD(trainExamples, learningInfo, persistWeightsAt)
+      elif learningInfo.optimizationAlgorithm == OptimizationAlgorithm.GRADIENT_DESCENT:
+        self.MinimizeLossWithGD(trainExamples, learningInfo, persistWeightsAt)
+
       # evaluate model on test set
-      (falsePositives, falseNegatives, truePositives, trueNegatives) = self.Evaluate(testExamples)
+      (falsePositives, falseNegatives, truePositives, trueNegatives, prob1) = self.Evaluate(testExamples, classThreshold)
       # aggregate results
       totalFalsePositives += falsePositives
       totalFalseNegatives += falseNegatives
